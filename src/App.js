b5bacac6814da8633f0e209a7fdcce0901e7193c
zwe linux <90806066+zwelinux@users.noreply.github.com>
2023-05-16 07:21:32 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <h2>Some Codes are Better Be Deployed!</h2>
     </div>
   );
 }
