9bef3cd040eea7c5265ecc6453fd78eea36059bc
zwe linux <90806066+zwelinux@users.noreply.github.com>
2023-05-16 07:28:26 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <h2>Some Codes are Better Be Deployed!</h2>
-      <code>Programming is funny</code>
-      <p>Fine ~ I make myself</p>
-      <small>ပြောင်းလဲတတ်တဲ့ မင်းကို တို့ဘယ်လိုမှ နားမလည် </small>
+      <h1>Hello React App</h1>
     </div>
   );
 }
