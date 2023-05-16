4fb228b359978ab31a9527224c5735a441865702
zwe linux <90806066+zwelinux@users.noreply.github.com>
2023-05-16 07:30:44 +0000

    Update App.js image updated with size 200 px

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
     <div className="App">
       <h1>Hello React App</h1>
       <p>Some Codes Are Better Be Deployed</p>
+      <img src='https://images.unsplash.com/photo-1661956601030-fdfb9c7e9e2f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=884&q=80' width='200px' />
     </div>
   );
 }
